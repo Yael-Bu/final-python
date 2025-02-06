dfeee6f1a1ac3395e71b364cb0f4824d971190b0
Yael Buchman <yael.tamir@gmail.com>
2025-02-06 16:02:34 +0200

    improve install dependency

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,18 +2,18 @@
 set -e
 
 # Update system
-sudo yum update -y
+sudo dnf update -y
 
 # Install Docker if not installed
 if ! command -v docker &> /dev/null
 then
-    sudo amazon-linux-extras enable docker
-    sudo yum install -y docker
+    sudo dnf install -y docker
     sudo systemctl start docker
     sudo systemctl enable docker
     sudo usermod -aG docker ec2-user
 fi
 
 # Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+DOCKER_COMPOSE_URL="https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)"
+sudo curl -L "$DOCKER_COMPOSE_URL" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
